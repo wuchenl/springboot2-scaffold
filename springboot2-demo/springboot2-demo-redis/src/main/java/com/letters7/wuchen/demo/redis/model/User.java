1fac96d9779a9fd7ec5e378217e6f7eecd44778d
wuchenl <wuchen@letters7.com>
2018-12-26 10:20:18 +0800

    新增测试模块。方便进行各种验证测试

--- springboot2-demo/springboot2-demo-redis/src/main/java/com/letters7/wuchen/demo/redis/model/User.java
+++ springboot2-demo/springboot2-demo-redis/src/main/java/com/letters7/wuchen/demo/redis/model/User.java
@@ -3,6 +3,8 @@ package com.letters7.wuchen.demo.redis.model;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 /**
  * @author wuchenl
  * @date 2018/12/20.
@@ -10,6 +12,7 @@ import lombok.Data;
 @Data
 @Builder
 public class User {
+    @NotNull(message="用户名不得为空！")
     private String userName;
 
     private String sex;

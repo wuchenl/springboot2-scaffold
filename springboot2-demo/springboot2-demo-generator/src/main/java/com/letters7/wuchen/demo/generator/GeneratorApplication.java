c87bf7f526da3a490dfe820c761ca45aa1c78e7a
wuchenl <wuchen@letters7.com>
2018-12-26 15:37:12 +0800

    新增代码自动生成模块，进行初始化。高优先级处理

---
+++ springboot2-demo/springboot2-demo-generator/src/main/java/com/letters7/wuchen/demo/generator/GeneratorApplication.java
@@ -0,0 +1,18 @@
+package com.letters7.wuchen.demo.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wuchen
+ * @version 0.1
+ * @date 2018/12/26 15:32
+ * @use
+ */
+@SpringBootApplication
+public class GeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeneratorApplication.class,args);
+    }
+}

fafd265503c6c4ad726eae394c719b8905e60c8f
wuchenl <wuchen@letters7.com>
2019-01-05 15:19:44 +0800

    新增文档模块

---
+++ doc/study.md
@@ -0,0 +1,4 @@
+#该文件为计划学习的东西目录  
+    1.spark系列  
+    2.Hadoop+storm+mapReduce 系列  
+    3.替换数据清洗部分代码
--- springboot2-demo/springboot2-demo-generator/src/test/com.letters7.wuchen.demo.generator/TestClass.java
+++ springboot2-demo/springboot2-demo-generator/src/test/com.letters7.wuchen.demo.generator/TestClass.java
@@ -1,6 +1,8 @@
 package com.letters7.wuchen.demo.generator;
 
+import com.alibaba.fastjson.JSON;
 import com.letters7.wuchen.demo.generator.model.DBType;
+import com.letters7.wuchen.demo.generator.model.GeneratorConfig;
 import org.junit.Test;
 
 import java.io.File;
@@ -21,9 +23,17 @@ public class TestClass {
 
         String path = Class.forName(DBType.MySQL.getDbClass()).getProtectionDomain().getCodeSource().getLocation().getPath();
         File file = new File(path);
-        String libPath= URLDecoder.decode(file.getCanonicalPath(), Charset.forName("UTF-8").displayName());
+        String libPath = URLDecoder.decode(file.getCanonicalPath(), Charset.forName("UTF-8").displayName());
         System.out.println(libPath);
     }
 
 
+    @Test
+    public void testNull() {
+        String json="{\"annotation\":false,\"annotationDAO\":false,\"comment\":false,\"jsr310Support\":false,\"needForUpdate\":false,\"needToStringHashcodeEquals\":false,\"offsetLimit\":false,\"overrideXML\":false,\"useActualColumnNames\":false,\"useDAOExtendStyle\":false,\"useExample\":false,\"useSchemaPrefix\":false,\"useTableNameAlias\":false}\n";
+        GeneratorConfig generatorConfig2 = GeneratorConfig.builder().build();
+        GeneratorConfig generatorConfig=JSON.parseObject(json,GeneratorConfig.class);
+        generatorConfig2.setProjectFolder(generatorConfig.getProjectFolder());
+        System.out.println(generatorConfig2);
+    }
 }
